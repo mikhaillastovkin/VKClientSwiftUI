b5c5ab0f622c2351904f30a437a121d067d31c43
Mikhail Lastovkin <m9503577335@gmail.com>
2022-03-29 15:53:06 +0300

    Initial Commit

---
+++ VKClientSwiftUI/VKClientSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKClientSwiftUIApp.swift
+//  VKClientSwiftUI
+//
+//  Created by Михаил Ластовкин on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKClientSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
